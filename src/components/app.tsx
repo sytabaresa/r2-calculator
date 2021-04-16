665a2b5decab37fae8ce884553a94ea2d08706d3
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2021-04-16 15:52:43 -0500

    clearup and add tailwind

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,19 +1,14 @@
 import { FunctionalComponent, h } from 'preact';
 import { Route, Router } from 'preact-router';
 
-import Home from '../routes/home';
-import Profile from '../routes/profile';
+import Calulator from '../routes/calculator';
 import NotFoundPage from '../routes/notfound';
-import Header from './header';
 
 const App: FunctionalComponent = () => {
     return (
         <div id="preact_root">
-            <Header />
             <Router>
-                <Route path="/" component={Home} />
-                <Route path="/profile/" component={Profile} user="me" />
-                <Route path="/profile/:user" component={Profile} />
+                <Route path="/" component={Calulator} />
                 <NotFoundPage default />
             </Router>
         </div>
---
+++ src/routes/calculator/index.tsx
@@ -0,0 +1,12 @@
+import { FunctionalComponent, h } from 'preact';
+
+const Calulator: FunctionalComponent = () => {
+    return (
+        <div>
+            <h1>Calculator</h1>
+            
+        </div>
+    );
+};
+
+export default Calulator;
