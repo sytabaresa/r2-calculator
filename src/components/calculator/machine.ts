8a52054fa9299fcf13c03878ec690fc29aa91695
Sebastian Yesid Tabares Amaya <sytabaresa@gmail.com>
2021-04-21 16:52:32 -0500

    add guards and validations

--- src/components/calculator/machine.ts
+++ src/components/calculator/machine.ts
@@ -22,6 +22,12 @@ const validUnOps: { [key: string]: (a: number) => number } = {
     '**2': (a) => a ** 2
 }
 
+const validOpsValid = (ctx: MachineContext, ev: MachineEvent): boolean =>
+    Object.keys(validOps).includes(ev.value)
+
+const validUnOpsValid = (ctx: MachineContext, ev: MachineEvent): boolean =>
+    Object.keys(validUnOps).includes(ev.value)
+
 const acuBuf = (ctx: MachineContext, ev: MachineEvent): MachineContext => {
     return { ...ctx, buf: (ctx.buf + ev.value).slice(0, 10) }
 }
@@ -31,11 +37,11 @@ const unFunc = (ctx: MachineContext, ev: MachineEvent): MachineContext => {
     return { ...ctx, buf: v + '', ans: v }
 }
 
-const AC = (): MachineContext => {
+const ACfunc = (): MachineContext => {
     return { buf: '', ans: 0, op: '' }
 }
 
-const C = (ctx: MachineContext): MachineContext => {
+const Cfunc = (ctx: MachineContext): MachineContext => {
     return { ...ctx, buf: '' }
 }
 
@@ -44,33 +50,38 @@ const machine = createMachine('normal', {
     normal: state(
         transition("NUM", "normal", reduce(acuBuf)),
         transition("OP", "accu",
-            reduce((ctx: MachineContext, ev: MachineEvent): MachineContext => {
+            guard(validOpsValid),
+            reduce((ctx, ev): MachineContext => {
                 return { ...ctx, buf: '', op: ev.value, ans: parseFloat(ctx.buf == '' ? '0' : ctx.buf) }
             })),
-        transition("UnOP", "result", reduce(unFunc)),
-        transition("AC", "normal", reduce(AC)),
-        transition("C", "normal", reduce(C)),
+        transition("UnOP", "result", guard(validUnOpsValid), reduce(unFunc)),
+        transition("AC", "normal", reduce(ACfunc)),
+        transition("C", "normal", reduce(Cfunc)),
     ),
     accu: state(
         transition("NUM", "accu", reduce(acuBuf)),
-        transition("OP", "accu", reduce((ctx: MachineContext, ev: MachineEvent): MachineContext => {
-            return { ...ctx, buf: '', op: ev.value, ans: validOps[ev.value](ctx.ans, parseFloat(ctx.buf == '' ? '0' : ctx.buf)) }
-        })),
+        transition("OP", "accu",
+            guard(validOpsValid),
+            reduce((ctx: MachineContext, ev: MachineEvent): MachineContext => {
+                return { ...ctx, buf: '', op: ev.value, ans: validOps[ev.value](ctx.ans, parseFloat(ctx.buf == '' ? '0' : ctx.buf)) }
+            })),
         transition("EVAL", "result", reduce((ctx: MachineContext, ev: MachineEvent): MachineContext => {
             const v = validOps[ctx.op](ctx.ans, parseFloat(ctx.buf == '' ? '0' : ctx.buf))
             return { ...ctx, buf: v + "", ans: v, op: '' }
         })),
-        transition("AC", "normal", reduce(AC)),
-        transition("C", "accu", reduce(C)),
+        transition("AC", "normal", reduce(ACfunc)),
+        transition("C", "accu", reduce(Cfunc)),
     ),
     result: state(
         transition("NUM", "normal", reduce(acuBuf)),
-        transition("OP", "accu", reduce((ctx: MachineContext, ev: MachineEvent): MachineContext => {
-            return { ...ctx, buf: '', op: ev.value }
-        })),
-        transition("AC", "normal", reduce(AC)),
-        transition("C", "result", reduce(C)),
-        transition("UnOP", "result", reduce(unFunc)),
+        transition("OP", "accu",
+            guard(validOpsValid),
+            reduce((ctx: MachineContext, ev: MachineEvent): MachineContext => {
+                return { ...ctx, buf: '', op: ev.value }
+            })),
+        transition("AC", "normal", reduce(ACfunc)),
+        transition("C", "result", reduce(Cfunc)),
+        transition("UnOP", "result", guard(validUnOpsValid), reduce(unFunc)),
     )
 
 }, el => el);
